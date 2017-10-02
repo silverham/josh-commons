89a016837dbcfb1ef96fbfdfb6bd547f53943a87
Joshua Graham <josh@joshuagraham.id.au>
2017-10-02 22:31:44 +1100

    Add compatibility for drupal multi-site - try to determine the URI from the filepath - not working - only works if site already has alaas.

--- Setup-Guides/git-drush-coder/drushrc.php
+++ Setup-Guides/git-drush-coder/drushrc.php
@@ -1,11 +1,17 @@
 <?php
 
-//enable drush to run outside docroot & set uri option (good for drush uil)
-//https://github.com/drush-ops/drush/issues/87#issuecomment-169266833
+/**
+ * @file
+ * Drush Runtime config.
+ *
+ * Enable drush to run outside docroot & set uri option (good for drush uli).
+ * @see https://github.com/drush-ops/drush/issues/87#issuecomment-169266833
+ */
 
-// grab $pre_aliases & $build_folders variables from aliases.drushrc.php in current dir;
+// Grab $pre_aliases & $build_folders variables from aliases.drushrc.php in
+// current dir.
 $path_to_alias_file = dirname(__FILE__) . '/aliases.drushrc.php';
-if(file_exists($path_to_alias_file)) {
+if (file_exists($path_to_alias_file)) {
   include $path_to_alias_file;
 }
 
@@ -14,13 +20,15 @@ if(file_exists($path_to_alias_file)) {
 $cwd = drush_cwd();
 
 // Compute list of root folder locations to
-// check if we are running in multi site and no uri passed
+// check if we are running in multi site and no uri passed.
 $roots = array();
 foreach ($pre_aliases as $alias) {
   $roots[] = $alias['root'];
 }
 
 $root_dups = array();
+// Returns an array using the values of array as keys and their
+// frequency in array as values.
 $root_counts = array_count_values($roots);
 foreach ($root_counts as $root_count_root => $root_count) {
   // False - not useable - multi site detected.
@@ -36,10 +44,10 @@ foreach ($pre_aliases as $alias) {
     // Set drupal context outside build folder (e.g. docroot).
     foreach ($build_folders as $folder) {
       // Composer project docroot/web.
-      if (file_exists("{$alias['root']}/{$folder}/web")) {
+      if (drush_valid_root("{$alias['root']}/{$folder}/web")) {
         $options['root'] = "{$alias['root']}/{$folder}/web";
       }
-      elseif (file_exists("{$alias['root']}/{$folder}")) {
+      elseif (drush_valid_root("{$alias['root']}/{$folder}")) {
         $options['root'] = "{$alias['root']}/{$folder}";
       }
     }
@@ -47,9 +55,29 @@ foreach ($pre_aliases as $alias) {
     // if it's usable "single" site.
     if ($root_dups[$alias['root']] === TRUE) {
       $options['uri'] = $alias['uri'];
-    } else {
-      var_dump("DUP");
+    }
+    else {
+      // Duplicate alias!
+      // If --uri or -l manully passed, then drush will handle uri.
+      // TL;DR; Do nothing here.
+      $args = drush_get_arguments();
+      if (!drush_get_option('uri') && !drush_sitealias_valid_alias_format($args[0])) {
+        $cwd = drush_cwd();
+        // If not, are inside the site specific folder?
+        // if (DIRECTORY_SEPARATOR is "\", and it is windows convert it.)
+        if (drush_is_windows() && strpos(DIRECTORY_SEPARATOR, '\\') !== FALSE) {
+          $cwd = str_replace('/', '\\', $cwd);
+        }
+        // Site is a string of the site path, false if not valid.
+        if ($site = drush_site_path($cwd)) {
+          // Yes - use that uri.
+          $uri = drush_sitealias_site_dir_from_filename($site . '/settings.php');
+          $options['uri'] = $uri;
+        }
+        else {
+          drush_log('WARNING! The current root has two or more aliases but none specified in the paramaters or no alias used! Pleaee Specify one, e.g. "--uri="mysite.com" or @mysite or run from [web_root]/sites/mysite/ folder. Continuing...', 'warning');
+        }
+      }
     }
   }
 }
-
